8d6e823b2f6f3c0b6ee004bab66c6dc39fc2b9e1
Jim Paine <17758778+JimPaine@users.noreply.github.com>
2022-07-07 09:44:49 +0100

    flattened animation blocks into original blocks

--- custom.ts
+++ custom.ts
@@ -10,46 +10,50 @@ let player: Sprite = null
 
 //% color=#667FFF icon="\uf073" block="Events"
 namespace events {
-    //% block="every $seconds seconds"
+    //% block="every %seconds seconds"
     //% seconds.min=0 seconds.max=5
     //% seconds.defl=5
     export function everyNSeconds(seconds: number, handler: () => void) {
         game.onUpdateInterval(seconds * 1000, handler);
     }
 
-    //% block="when player catches $type item"
+    //% block="when player catches %type item"
     //% type.shadow="MovingItemType.Nice"
     export function whenPlayerCatchesItem(type: MovingItemType, handler: () => void) {
-        let kind = SpriteKind.Food
+        let kind = SpriteKind.Food;
 
         if (type == MovingItemType.Bad) {
-            kind = SpriteKind.Enemy
+            kind = SpriteKind.Enemy;
         }
 
         sprites.onOverlap(SpriteKind.Player, kind, function (sprite, otherSprite) {
-            otherSprite.destroy()
+            otherSprite.destroy();
             handler();
-        })
+        });
     }
 }
 
 //% color=#FF4D88 icon="\uf443" block="Characters"
 namespace characters {
-    //% block="create $image | which is $type | moving left: $left | moving right: $right"
+    //% block="create character $image which is %type||left movement %left right movement %right"
     //% image.shadow="screen_image_picker"
     //% type.shadow="MovingItemType.Nice"
     //% left.shadow="animation_editor"
     //% right.shadow="animation_editor"
-    export function createAnimatedMovingItem(image = img``, type: MovingItemType, left = [img``], right = [img``]) {
-        let kind = SpriteKind.Food
+    //% inlineInputMode=inline
+    //% expandableArgumentMode=toggle
+    export function createMovingItem(image: Image, type: MovingItemType, left?: Image[], right?: Image[]) {
+        let kind = SpriteKind.Food;
 
         if (type == MovingItemType.Bad) {
-            kind = SpriteKind.Enemy
+            kind = SpriteKind.Enemy;
         }
 
-        let x = randint(-50, 50)
-        let sprite = sprites.createProjectileFromSide(image,x,randint(-50, 50))
-        sprite.setKind(kind)
+        let x = randint(-50, 50);
+        let sprite = sprites.createProjectileFromSide(image,x,randint(-50, 50));
+        sprite.setKind(kind);
+
+        if (left == null || right == null) return;
 
         if (x < 0) {
             animation.runImageAnimation(
@@ -57,50 +61,36 @@ namespace characters {
                 left,
                 200,
                 true
-            )
+            );
         } else {
             animation.runImageAnimation(
                 sprite,
                 right,
                 200,
                 true
-            )
+            );
         }
     }
 
-    //% block="create $image | which is $type"
-    //% image.shadow="screen_image_picker"
-    //% type.shadow="MovingItemType.Nice"
-    export function createMovingItem(image = img``, type: MovingItemType) {
-        let kind = SpriteKind.Food
-
-        if (type == MovingItemType.Bad) {
-            kind = SpriteKind.Enemy
-        }
-        sprites.createProjectileFromSide(image,randint(-50, 50),randint(-50, 50)).setKind(kind)
-    }
-
-    //% block="Create player $image"
-    //% image.shadow="screen_image_picker"
-    export function createPlayer(image = img``) {
-        player = sprites.create(image, SpriteKind.Player);
-        player.setStayInScreen(true);
-    }
-
-    //% block="Create animated player $image | moving left: $left | moving right: $right"
+    //% block="Create player %image || left movement: %left right movement: %right"
     //% image.shadow="screen_image_picker"
     //% left.shadow="animation_editor"
     //% right.shadow="animation_editor"
-    export function createAnimatedPlayer(image = img``, left = [img``], right = [img``]) {
+    //% inlineInputMode=inline
+    //% expandableArgumentMode=toggle
+    export function createPlayer(image: Image, left?: Image[], right?: Image[]) {
         player = sprites.create(image, SpriteKind.Player);
         player.setStayInScreen(true);
+
+        if (left == null || right == null) return;
+
         controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function() {
             animation.runImageAnimation(
                 player,
                 left,
                 200,
                 true
-            )
+            );
         });
 
         controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function() {
@@ -109,16 +99,16 @@ namespace characters {
                 right,
                 200,
                 true
-            )
+            );
         });
 
         controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Released, function () {
-            animation.stopAnimation(animation.AnimationTypes.All, player)
-        })
+            animation.stopAnimation(animation.AnimationTypes.All, player);
+        });
 
         controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Released, function () {
-            animation.stopAnimation(animation.AnimationTypes.All, player)
-        })
+            animation.stopAnimation(animation.AnimationTypes.All, player);
+        });
     }
 }
 
